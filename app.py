9bd1fe5d249580b7980e9a4a6b28c6151b90a7c3
Suguru Asai <sugutu@asaism.com>
2018-04-25 17:49:27 +0900

    - Added work visualization mode.

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    notyet = 10
+    processing = 20
+    processed = 30
+    done = 40
+    total = 100
+
+    return render_template('index.html',
+                           total=total,
+                           notyet=notyet,
+                           processing=processing,
+                           processed=processed,
+                           done=done)
 
 
 if __name__ == '__main__':
